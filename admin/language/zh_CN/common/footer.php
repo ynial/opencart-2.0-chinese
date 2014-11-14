8ee9e6782ffb1fcd465300365bad45cee6a3cb2b
huiztechyangning <925291@qq.com>
2014-11-14 14:37:50 +0800

    添加商品汉化
    修改原有的错误

--- admin/language/zh_CN/common/footer.php
+++ admin/language/zh_CN/common/footer.php
@@ -1,3 +1,3 @@
 <?php
-$_['text_footer']      = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' 版权所有';
+$_['text_footer']      = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' 版权所有<br />';
 $_['text_version']      = '版本 %s';
