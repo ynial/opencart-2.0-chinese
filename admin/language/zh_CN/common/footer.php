bcb8ae3564d17e96d2b1fdd6e5ee25609d7bade4
huiztechyangning <925291@qq.com>
2014-11-14 09:57:01 +0800

    footer
    错误

--- admin/language/zh_CN/common/footer.php
+++ admin/language/zh_CN/common/footer.php
@@ -1,3 +1,3 @@
 <?php
-$_['text_footer']      = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' 版权所有.<br />版本 %s';
+$_['text_footer']      = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' 版权所有';
 $_['text_version']      = '版本 %s';
